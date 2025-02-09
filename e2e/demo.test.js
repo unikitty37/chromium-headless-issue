0ae4322abe0f0e600324d822e261f4c537b0007a
John Yeates <unikitty37@users.noreply.github.com>
2025-02-09 12:46:03 +0000

    Add button and tests

--- e2e/demo.test.js
+++ e2e/demo.test.js
@@ -1,6 +1,22 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from '@playwright/test'
 
-test('home page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator('h1')).toBeVisible();
-});
+test.describe('Full screen control', () => {
+  test.describe('when the browser is not full screen', () => {
+    test('is displayed', async ({ page }) => {
+      await page.goto('/')
+
+      await expect(page.getByRole('button', { name: 'Full Screen' })).toBeVisible()
+    })
+  })
+
+  test.describe('when the browser is full screen', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/')
+      await page.evaluate(() => document.documentElement.requestFullscreen())
+    })
+
+    test('is not displayed', async ({ page }) => {
+      await expect(page.getByRole('button', { name: 'Full Screen' })).not.toBeVisible()
+    })
+  })
+})
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { browser } from '$app/environment'
+  import { MediaQuery } from 'svelte/reactivity'
+
+  const isFullScreen = new MediaQuery('display-mode: fullscreen')
+
+  const goFullscreen = () => {
+    if (!browser) return
+
+    document.documentElement.requestFullscreen()
+  }
+</script>
+
+<h1>Push the button, Frank</h1>
+
+{#if !isFullScreen.current}
+  <button onclick={goFullscreen}>Full Screen</button>
+{/if}
